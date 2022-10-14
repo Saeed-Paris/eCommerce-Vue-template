c1a2bd2d2124a59eaf14a27a173725daee3f30d1
Saeed-Paris <saeed.paris.0026@gmail.com>
2022-10-14 17:11:04 +0330

    ➕Add SVGs ➕

---
+++ src/components/shared/SearchBar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import SearchSvg from "../svg/SearchSvg.vue";
+</script>
+
+<template>
+  <div class="relative border-b-2 h-fit">
+    <SearchSvg class="absolute left-0" />
+    <input type="text" />
+  </div>
+</template>
+
+<style scoped></style>

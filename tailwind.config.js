bbaf2a5ee56495eb2e4ae7afaaa37cb708886f7c
Saeed-Paris <saeed.paris.0026@gmail.com>
2022-10-14 19:38:24 +0330

    :pencil2: tailwind config :pencil2:

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      purple: "#3f3cbb",
+      midnight: "#121063",
+      metal: "#565584",
+      tahiti: "#3ab7bf",
+      silver: "#ecebff",
+      "bubble-gum": "#ff77e9",
+      bermuda: "#78dcca",
+      "--c-primary": "#5ece7b",
+      "--c-primary-variant": "#9EE2B0",
+      "--c-secondary": "#1D1F22",
+      "--c-secondary-variant": "#43464E",
+    },
     extend: {},
   },
   plugins: [],

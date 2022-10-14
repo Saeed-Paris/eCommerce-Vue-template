f25a8b68719f1e611e583dc105d0154e12a088f2
Saeed-Paris <saeed.paris.0026@gmail.com>
2022-10-14 23:07:10 +0330

    🔧Tailwind Config🔧

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,36 @@ module.exports = {
       "--c-secondary": "#1D1F22",
       "--c-secondary-variant": "#43464E",
     },
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        // Simple 8 row grid
+        8: "repeat(8, minmax(0, 1fr))",
+        9: "repeat(9, minmax(0, 1fr))",
+        10: "repeat(10, minmax(0, 1fr))",
+        11: "repeat(11, minmax(0, 1fr))",
+        12: "repeat(12, minmax(0, 1fr))",
+        13: "repeat(13, minmax(0, 1fr))",
+
+        // Complex site-specific row configuration
+        // 'layout': '200px minmax(900px, 1fr) 100px',
+      },
+      gridRowStart: {
+        8: "8",
+        9: "9",
+        10: "10",
+        11: "11",
+        12: "12",
+        13: "13",
+      },
+      gridRowEnd: {
+        8: "8",
+        9: "9",
+        10: "10",
+        11: "11",
+        12: "12",
+        13: "13",
+      },
+    },
   },
   plugins: [],
 };

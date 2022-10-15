d8c54e4d7bf8910714d25fa21bc699d8c41e8f00
Saeed-Paris <saeed.paris.0026@gmail.com>
2022-10-15 21:27:49 +0330

    🧱import Product slider 🧱

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,13 @@
 //@ts-ignore
 import SwiperBanner from "../components/home/SwiperBanner.vue";
 import CategorySection from "../components/home/CategorySection.vue";
+import ProductSlider from "../components/home/ProductSlider.vue";
 </script>
 
 <template>
   <SwiperBanner />
   <CategorySection />
+  <ProductSlider />
 </template>
 
 <style scoped></style>

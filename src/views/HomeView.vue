47ff8cf73fcf5a92693f7f2a6defbd62e5311dd1
Saeed-Paris <saeed.paris.0026@gmail.com>
2022-10-15 21:31:52 +0330

    🧱import newsletter to home 🧱

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,14 @@
 import SwiperBanner from "../components/home/SwiperBanner.vue";
 import CategorySection from "../components/home/CategorySection.vue";
 import ProductSlider from "../components/home/ProductSlider.vue";
+import NewsLetter from "../components/home/NewsLetter.vue";
 </script>
 
 <template>
   <SwiperBanner />
   <CategorySection />
   <ProductSlider />
+  <NewsLetter />
 </template>
 
 <style scoped></style>

1ca0c91eb25f5a20b245767bcc6fa3d7118fbec9
Saeed-Paris <saeed.paris.0026@gmail.com>
2022-10-19 20:47:44 +0330

    🧱 Add footer 🧱

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import MainHeader from "./components/shared/MainHeader.vue";
+import MainFooter from "./components/shared/MainFooter.vue";
 </script>
 
 <template>
@@ -8,6 +9,9 @@ import MainHeader from "./components/shared/MainHeader.vue";
   <main class="m-auto w-10/12">
     <RouterView />
   </main>
+  <footer class="-bg--c-dark-variant">
+    <MainFooter class="m-auto w-10/12" />
+  </footer>
 </template>
 
 <style scoped></style>
---
+++ src/components/shared/MainFooter.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+let footerItems = [
+  {
+    title: "درباره ما",
+    items: ["ما کی هستیم", "کیفیت در جزيیات", "نظرات مشتری"],
+  },
+  {
+    title: "DEPARTMENTS",
+    items: ["Women fashion", "Kidswear", "Home"],
+  },
+  {
+    title: "HELP",
+    items: ["خدمات مشتری", "راهنمای سایز", "تماس با ما"],
+  },
+  {
+    title: "خرید و تحویل سفارش",
+    items: ["شرایط خرید", "گارانتی", "تماس با ما"],
+  },
+];
+</script>
+
+<template>
+  <section class="text-white">
+    <div class="flex justify-between">
+      <div
+        class="flex flex-col items-center"
+        v-for="(item, i) in footerItems"
+        :key="i"
+      >
+        <h4>{{ item.title }}</h4>
+        <router-link to="" v-for="(subItem, j) in item.items" :key="j">{{
+          subItem
+        }}</router-link>
+      </div>
+    </div>
+    <div></div>
+  </section>
+</template>
+
+<style scoped></style>
